453d0e54359f8ac9b375b5979327ff601fbaa4ce
Mohamed Hamdy <mohamed.hamdy.sp@outlook.com>
2024-09-14 04:15:42 +0300

    Added basic_operations.py script to perform arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# Define two variables
+number1 = 10
+number2 = 5
+# Perform basic arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+# Print the results
+print(f"addition of {number1} and {number2} is {addition}")
+print(f"subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
